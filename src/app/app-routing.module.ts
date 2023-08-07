7333ec35dd4cb10581f12506d31c21010af7fdfe
captainlomik <nastiy248@mail.ru>
2023-08-07 17:18:19 +0300

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './main/main-page/main-page.component';
 import { CatalogPageComponent } from './main/catalog-page/catalog-page.component';
 import { ContactPageComponent } from './main/contact-page/contact-page.component';
+import { LayoutComponent } from './main/layout/layout.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', component: MainPageComponent },
-  { path: 'catalog', component: CatalogPageComponent },
-  { path: 'contact', component: ContactPageComponent }
+  {
+    path: '', component: LayoutComponent, children: [
+      {path:'', component:MainPageComponent},
+      { path: 'catalog', component: CatalogPageComponent },
+      { path: 'contact', component: ContactPageComponent },
+    ]
+  },
+  { path: '404', component: NotFoundComponent }
 
 ];
 
@@ -15,5 +22,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { 
+export class AppRoutingModule {
 }
