cf2b823f4da746557f1dcc4e09b470d45e371e7f
captainlomik <nastiy248@mail.ru>
2023-08-07 17:16:29 +0300

    add modal

--- src/app/main/main-page/main-page.component.ts
+++ src/app/main/main-page/main-page.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild, ViewContainerRef, ViewEncapsulation } from '@angular/core';
+import { ModalService } from 'src/app/shared/modal/modal.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MainPageComponent {
   
+  constructor(private modalService: ModalService) {}
+
+  openModal(modalTemplate: TemplateRef<any>) {
+    this.modalService
+      .open(modalTemplate)
+      // .subscribe((action) => {
+      //   console.log('modalAction', action);
+      // });
+  }
+
 }
