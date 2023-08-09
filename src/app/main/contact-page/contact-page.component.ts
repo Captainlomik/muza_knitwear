1a7f9e0c8d63161e142508af90cab39689c5e6e1
captainlomik <nastiy248@mail.ru>
2023-08-09 23:08:25 +0300

    add connect with db

--- src/app/main/contact-page/contact-page.component.ts
+++ src/app/main/contact-page/contact-page.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { QuestionService } from 'src/app/shared/services/question.service';
 
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
   styleUrls: ['./contact-page.component.scss']
 })
-export class ContactPageComponent {
+export class ContactPageComponent implements OnInit {
+
+  questionForm!: FormGroup
+  constructor(private questionService: QuestionService) { }
+
+  ngOnInit(): void {
+    this.questionForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      phone: new FormControl('', Validators.required),
+      comment: new FormControl('', Validators.required)
+    })
+  }
+
+  submitForm() {
+    this.questionService.post(this.questionForm.value).subscribe(
+      {
+        next: el => console.log(el),
+        error: er => console.log(er)
+      }
+    )
+  }
 
 }
