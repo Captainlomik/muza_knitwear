3c811fb5dc6fd77afb16c5a6683bd3a6c221aeb8
captainlomik <nastiy248@mail.ru>
2023-08-09 23:07:55 +0300

    add module http

--- src/app/main/main-page/main-page.component.ts
+++ src/app/main/main-page/main-page.component.ts
@@ -1,6 +1,7 @@
-import { Component, TemplateRef, ViewChild, ViewContainerRef, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild, ViewContainerRef, ViewEncapsulation } from '@angular/core';
 import { ModalService } from 'src/app/shared/modal/modal.service';
-import { Subscription } from 'rxjs';
+import { PersonalOrderService } from 'src/app/shared/services/personal-order.service';
+import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-main-page',
@@ -8,16 +9,43 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./main-page.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class MainPageComponent {
-  
-  constructor(private modalService: ModalService) {}
+export class MainPageComponent implements OnInit {
+
+  orderForm!: FormGroup
+
+  constructor(private modalService: ModalService, private personalOrder: PersonalOrderService) {
+  }
+
+  ngOnInit(): void {
+    this.orderForm = new FormGroup({
+      name: new FormControl(' ', Validators.required),
+      phone: new FormControl(' ', Validators.required),
+    })
+  }
+
 
   openModal(modalTemplate: TemplateRef<any>) {
     this.modalService
       .open(modalTemplate)
-      // .subscribe((action) => {
-      //   console.log('modalAction', action);
-      // });
+      .subscribe((action) => {
+        console.log('modalAction', action);
+      });
+  }
+
+  submitOrder() {
+    // let order = {
+    //   id: 1,
+    //   data: Date.now(),
+    //   name: this.orderForm.value.name,
+    //   phone: this.orderForm.value.phone
+    // }
+    // this.personalOrder.post(this.orderForm.value).subscribe({
+    //   next: el => console.log(el),
+    //   error: er => console.log(er)
+    // })
+    this.modalService.submitModal()
+    console.log('lol')
+    console.log(this.orderForm.value)
   }
 
 }
