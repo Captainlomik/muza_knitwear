d685edba9fdacfe46027664ea9dfb8c56769567e
captainlomik <nastiy248@mail.ru>
2023-08-12 17:46:52 +0300

    add main info

--- src/app/admin/settings-page/settings-page.component.ts
+++ src/app/admin/settings-page/settings-page.component.ts
@@ -13,30 +13,35 @@ export class SettingsPageComponent {
   constructor(private mainService: MainInfoService) {
     this.settingsForm = new FormGroup({
       "aboutMe": new FormControl('', Validators.required),
-      "names": new FormArray([new FormControl('', Validators.required)]),
-      "texts": new FormArray([new FormControl('', Validators.required)]),
+      // "names": new FormArray([new FormControl('', Validators.required)]),
+      // "texts": new FormArray([new FormControl('', Validators.required)]),
+      "vk": new FormControl('', Validators.required),
+      "instagram": new FormControl('', Validators.required),
+      "telegram": new FormControl('', Validators.required),
+      'phone': new FormControl('', Validators.required),
+      'phoneSrc': new FormControl('', Validators.required),
+      'mail': new FormControl('', [Validators.required, Validators.email]),
     })
   }
 
-  getNames(): FormArray {
-    return this.settingsForm.controls["names"] as FormArray
-  }
-  getTexts(): FormArray {
-    return this.settingsForm.controls["texts"] as FormArray
-  }
+  // getNames(): FormArray {
+  //   return this.settingsForm.controls["names"] as FormArray
+  // }
+  // getTexts(): FormArray {
+  //   return this.settingsForm.controls["texts"] as FormArray
+  // }
 
-  addInfo() {
-    (<FormArray>this.settingsForm.controls["names"]).push(new FormControl('', Validators.required));
-    (<FormArray>this.settingsForm.controls["texts"]).push(new FormControl('', Validators.required))
-  }
+  // addInfo() {
+  //   (<FormArray>this.settingsForm.controls["names"]).push(new FormControl('', Validators.required));
+  //   (<FormArray>this.settingsForm.controls["texts"]).push(new FormControl('', Validators.required))
+  // }
 
 
   submitSettings() {
-    this.mainService.post(this.settingsForm.value).subscribe({
+    this.mainService.put(this.settingsForm.value, 1).subscribe({
       next: () => console.log('done'),
       error: er => console.log(er)
     })
-    this.settingsForm.reset()
   }
 
 }
