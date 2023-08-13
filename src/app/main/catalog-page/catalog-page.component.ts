7bd2b9d386a5b0c027c059be2d79cf183b40895e
captainlomik <nastiy248@mail.ru>
2023-08-14 01:07:13 +0300

    add catalog page

--- src/app/main/catalog-page/catalog-page.component.ts
+++ src/app/main/catalog-page/catalog-page.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Collection } from 'src/app/shared/interface';
+import { CollectionService } from 'src/app/shared/services/collection.service';
 
 @Component({
   selector: 'app-catalog-page',
   templateUrl: './catalog-page.component.html',
   styleUrls: ['./catalog-page.component.scss']
 })
-export class CatalogPageComponent {
+export class CatalogPageComponent implements OnInit {
+
+  collections: Collection[] = []
+
+  constructor(private collectionsService: CollectionService) { }
+
+  ngOnInit(): void {
+    this.collectionsService.get().subscribe({
+      next: el => this.collections = el,
+      error: er => console.log(er)
+    })
+  }
 
 }
