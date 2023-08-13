15ca1e92fad06017828555cdc827f536f8344c0f
captainlomik <nastiy248@mail.ru>
2023-08-14 01:07:26 +0300

    add catalog block

--- src/app/main/main-page/main-page.component.ts
+++ src/app/main/main-page/main-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, TemplateRef, ViewChild, ViewContainerRef, ViewEncaps
 import { ModalService } from 'src/app/shared/modal/modal.service';
 import { PersonalOrderService } from 'src/app/shared/services/personal-order.service';
 import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { CollectionService } from 'src/app/shared/services/collection.service';
+import { Collection } from 'src/app/shared/interface';
 
 @Component({
   selector: 'app-main-page',
@@ -12,14 +14,22 @@ import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms'
 export class MainPageComponent implements OnInit {
 
   orderForm!: FormGroup
+  collections: Collection[] = []
 
-  constructor(private modalService: ModalService, private personalOrder: PersonalOrderService) {
+  constructor(private modalService: ModalService,
+    private personalOrder: PersonalOrderService,
+    private collectionsService: CollectionService) {
   }
 
   ngOnInit(): void {
     this.orderForm = new FormGroup({
       name: new FormControl(' ', Validators.required),
       phone: new FormControl(' ', Validators.required),
+    })
+
+    this.collectionsService.get().subscribe({
+      next: el => this.collections = el,
+      error: er => console.log(er)
     })
   }
 
